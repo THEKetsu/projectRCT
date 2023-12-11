54a3c2f0c6bc40664a302d6bb7cfa97c003a9fa5
Aimasu59 <corentintyteca@gmail.com>
2023-12-11 13:56:35 +0100

    debut menu deroulant bas

---
+++ front-end/components/BottomBar/bottomBarStyle.js
@@ -0,0 +1,61 @@
+import { StyleSheet } from 'react-native'
+// Styles pour les éléments du composant
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      width: '100%',
+      height: '10%'
+  
+    },
+    bar: {
+      display: "flex",
+      flexDirection: "row",
+      backgroundColor: 'lightblue',
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      
+    },
+    button: {
+      position: "absolute",
+      top: '-10%',
+      backgroundColor: 'lightblue',
+      width: 100,
+      height: 40,
+      borderRadius: 20,
+      alignItems: 'center'
+    },
+    arrowImage: {
+      width: '10%',
+      height: '40%'
+  
+    },
+    eraserImage: {
+      width: '10%',
+      height: '40%'
+  
+    },
+    penImage: {
+      width: '10%',
+      height: '40%'
+  
+    },
+    maillotImage: {
+      width: '10%',
+      height: '40%'
+  
+    },
+    ballImage: {
+      width: '10%',
+      height: '40%'
+  
+    },
+    displayBottomImage: {
+      width: '15%',
+      height: '40%'
+  
+    }
+  });
+  
--- front-end/screens/Home.tsx
+++ front-end/screens/Home.tsx
@@ -1,10 +1,12 @@
 import {View, Text, Button} from 'react-native';
+import AnimatedBottomBar from '../components/BottomBar/BottomBar';
 
 export default function Home () {
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>SALUT</Text>
+            <AnimatedBottomBar></AnimatedBottomBar>
+
         </View>
     )
 }
