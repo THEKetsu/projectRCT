a13949a2e125221c73e33a1cb47ef058b6d0fd39
thomaschok <thomas.choquell@isen.yncrea.fr>
2023-12-11 14:17:39 +0100

    bouton pages

---
+++ front-end/components/MenuPosition/style.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    pageContainer: {
+      width: "100%",
+    },
+    pageContent: {
+      flex: 1,
+      backgroundColor: 'white',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: 16,
+      width: '100%',
+    },
+    scrollContainer: {
+      flexDirection: 'row',
+    },
+    navigation: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingVertical: 8,
+    },
+  });
+  
--- front-end/screens/Home.tsx
+++ front-end/screens/Home.tsx
@@ -1,10 +1,14 @@
+import React from "react";
 import {View, Text, Button} from 'react-native';
+import MenuPosition from '../components/MenuPosition/MenuPosition';
+
 
 export default function Home () {
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>SALUT</Text>
+            <MenuPosition />
         </View>
     )
 }
