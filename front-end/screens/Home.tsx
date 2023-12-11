b24fa5d5199f573894c66542af9b0bd7a2eed1b5
thomaschok <thomas.choquell@isen.yncrea.fr>
2023-12-11 15:18:10 +0100

    oiefrjoe

--- front-end/screens/Home.tsx
+++ front-end/screens/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {View, Text, Button} from 'react-native';
-import AnimatedBottomBar from '../components/BottomBar/BottomBar'import SVGField from '../components/utils/SVGField';
+import AnimatedBottomBar from '../components/BottomBar/BottomBar';
+import SVGField from '../components/utils/SVGField';
 
 export default function Home () {
 
@@ -8,8 +9,6 @@ export default function Home () {
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <SVGField height={300} width={300} />
             <AnimatedBottomBar></AnimatedBottomBar>
-
-
         </View>
     )
 }
