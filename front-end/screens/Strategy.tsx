f5dd5373258839936730b3f5c6bd0c63cd126c67
BENHDACH <mohamed.benhdach@isen.yncrea.fr>
2024-02-20 10:12:04 +0100

    Fix refresh, update

--- front-end/screens/Strategy.tsx
+++ front-end/screens/Strategy.tsx
@@ -5,52 +5,39 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import ViewShot from 'react-native-view-shot';
 import { Field } from '../components/Field/Field';
 import Position from '../components/ToolBar/Position';
-import * as FileSystem from 'expo-file-system';
 
 export default function Strategy() {
   const viewShotRef = useRef<ViewShot>(null);
   let capturedCount = 0;
   const [base64Icon, setBase] = useState('');
+  const [videoURL, setVideoURL] = useState('');
 
-//   const captureScreen = async () => {
-//     try {
-//       //@ts-ignore
-//       const result = await viewShotRef.current.capture();
-//       console.log('Capture successful', result, capturedCount);
-//       capturedCount = capturedCount + 1;
-//       setBase(result);
+  const captureScreen = async () => {
+    try {
+      //@ts-ignore
+      const result = await viewShotRef.current.capture();
+      console.log('Capture successful', result, capturedCount);
+      capturedCount = capturedCount + 1;
+      setBase(result);
 
-//       if (capturedCount < 3) {
-//         setTimeout(captureScreen, 1000);
-//       }
+      if (capturedCount < 3) {
+        setTimeout(captureScreen, 1000);
+      } else {
+        // Create the video here
+        const images = [base64Icon]; // Use an array with your captured images (base64 strings)
 
-//       // Save the captured image to a file
-//     //   saveImageToFile(result);
-//     } catch (error) {
-//       console.error('Capture failed', error);
-//     }
-//   };
+      }
+    } catch (error){
 
-//   const saveImageToFile = async (base64Image: string) => {
-//     try {
-//       const path = FileSystem.documentDirectory + 'myImage.png';
-
-//       await FileSystem.writeAsStringAsync(path, base64Image, {
-//         encoding: FileSystem.EncodingType.Base64,
-//       });
-
-//       console.log('Image saved successfully at:', path);
-//     } catch (error) {
-//       console.error('Failed to save image:', error);
-//     }
-//   };
+    }
+  };
 
   return (
     <View style={styles.container}>
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <ViewShot ref={viewShotRef} options={{ format: 'png', quality: 0.9 }}>
+        {/* <ViewShot ref={viewShotRef} options={{ format: 'png', quality: 0.9 }}> */}
           <Field />
-        </ViewShot>
+        {/* </ViewShot> */}
 
         <Position />
         {/* <Image style={{ width: 50, height: 50 }} source={{ uri: base64Icon }} /> */}
@@ -61,13 +48,16 @@ export default function Strategy() {
         style={({ pressed }) => [
           {
             backgroundColor: pressed ? 'red' : 'rgb(165, 15, 25)',
+            position: 'absolute',
             padding: 10,
             borderRadius: 5,
             top: 20,
           },
         ]}
       >
+
       </Pressable> */}
+
     </View>
   );
 }
