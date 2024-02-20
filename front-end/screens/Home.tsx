612fa309d2f711d465c50004505d17aa5757c3a4
Hugo30128 <hgouiran@gmail.com>
2024-02-20 14:20:10 +0100

    Revert "Merge branch 'review' of https://github.com/THEKetsu/projectRCT into dev"
    This reverts commit 57389165c799a8f93168fcbfbae907521b1f4e02, reversing
    changes made to 83094ab34ada9297dafc4d476d2b918039aabb96.

--- front-end/screens/Home.tsx
+++ front-end/screens/Home.tsx
@@ -1,15 +1,52 @@
-import {View, Button} from 'react-native';
-import {useEffect} from "react";
-import Player from '../classes/Player';
-import Ballon from '../classes/Ballon';
-import React from 'react';
+import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import styles from '../components/drawer/styles';
+import TopWidget from '../components/drawer/TopWidget';
+import DrawerLeft from '../components/drawer/DrawerLeft';
 
-// @ts-ignore
-export default function Home ({ navigation }) {
 
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Button onPress={() => navigation.navigate("Strategy")} title={"GO TO STRATEGY"}/>
-        </View>
-    )
+export default function Home() {
+    console.log('******************************TEST*********************************');
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  /**
+   * Handle the press event on the play button.
+   *
+   * @param {any} info - the information related to the press event
+   * @return {void} 
+   */
+  const handlePressPlayButton = (info: any) => {
+    setIsDrawerOpen(true);
+  };
+
+  /**
+   * Closes the drawer by setting the isDrawerOpen state to false.
+   *
+   * @param {} 
+   * @return {}
+   */
+  const handleCloseDrawer = () => {
+    setIsDrawerOpen(false);
+  };
+  /**
+   * A function that handles the selected item.
+   *
+   * @param {any | null} selectedItem - the selected item to be handled
+   * @return {void} 
+   */
+  const handleItemSelected = (selectedItem: any | null) => { 
+    setSelectedItem(selectedItem.name);
+  };
+
+  
+  console.log('Information about the selected item: ', selectedItem);
+  return (
+    <View style={styles.container}>
+      {isDrawerOpen && <DrawerLeft onClose={handleCloseDrawer} isOpen={isDrawerOpen} onItemSelected={handleItemSelected} />}
+      {!isDrawerOpen && <TopWidget onPlayButtonPress={handlePressPlayButton}  selectedItem={selectedItem}/>}
+      <View style={styles.home}>
+        <Text></Text>
+      </View>
+    </View>
+  );
 }
