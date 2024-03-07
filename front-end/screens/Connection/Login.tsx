6848f6e1f55f6d15df964a3c0d2db612d56c471e
Aimasu59 <corentintyteca@gmail.com>
2024-03-07 18:05:56 +0100

    merge de phiphi dans main

--- front-end/screens/Connection/Login.tsx
+++ front-end/screens/Connection/Login.tsx
@@ -17,6 +17,8 @@ export default function Login () {
   const [password, setPassword] = React.useState('');
   const [isEmailValid, setIsEmailValid] = React.useState(false); // New state to track email validity
   const [isPasswordValid, setIsPasswordValid] = React.useState(false); // New state to track password validity
+  const [errorText, setErrorText] = React.useState('');
+  const [errorOccurred, setErrorOccurred] = React.useState(false);
 
 
   const login = () => {
@@ -42,6 +44,8 @@ export default function Login () {
       })
       .catch((error) => {
         console.log(error);
+        setErrorOccurred(true);
+        setErrorText("Erreur de connexion. Vérifiez votre e-mail et votre mot de passe.");
       })
   };
 
@@ -72,29 +76,34 @@ export default function Login () {
             <Image source={require('../../assets/star.png')} style={[styles.starImage, { width: starSize, height: starSize }]} />
           </View>
           <View style={styles.overlay}>
-              <TextInput
-                  style={styles.input}
-                  placeholder="E-mail"
-                  placeholderTextColor="rgba(255, 255, 255, 0.5)" // Couleur du placeholder avec opacité
-                  underlineColorAndroid="transparent"
-                  onChangeText={(text) => {
-                    setEmail(text);
-                    setIsEmailValid(regex.test(text)); // Update email validity
-                  }}
-              />
-            </View>
-            <View style={[styles.overlay, styles.overlay2]}>
-              <TextInput
-                  style={styles.input}
-                  placeholder="Mot de passe"
-                  placeholderTextColor="rgba(255, 255, 255, 0.5)" // Couleur du placeholder avec opacité
-                  underlineColorAndroid="transparent"
-                  secureTextEntry={true}
-                  onChangeText={(text) => {
-                    setPassword(text);
-                    setIsPasswordValid(text.length >= 6); // Update password validity
-                  }}
-              />
+            <TextInput
+              style={[styles.input, errorOccurred && styles.errorInput]} // Appliquer le style d'erreur si une erreur est survenue
+              placeholder="E-mail"
+              placeholderTextColor={errorOccurred ? '#FF0000' : 'rgba(255, 255, 255, 0.5)'} // Changer la couleur du placeholder en rouge si une erreur est survenue
+              underlineColorAndroid={errorOccurred ? '#FF0000' : 'transparent'} // Changer la couleur du contour en rouge si une erreur est survenue
+              onChangeText={(text) => {
+                  setEmail(text);
+                  setIsEmailValid(regex.test(text));
+              }}
+            />
+          </View>
+          <View style={[styles.overlay, styles.overlay2]}>
+            <TextInput
+                style={[styles.input, errorOccurred && styles.errorInput]}
+                placeholder="Mot de passe"
+                placeholderTextColor={errorOccurred ? '#FF0000' : 'rgba(255, 255, 255, 0.5)'} // Changer la couleur du placeholder en rouge si une erreur est survenue
+                underlineColorAndroid={errorOccurred ? '#FF0000' : 'transparent'} // Changer la couleur du contour en rouge si une erreur est survenue
+                secureTextEntry={true}
+                onChangeText={(text) => {
+                  setPassword(text);
+                  setIsPasswordValid(text.length >= 6); // Update password validity
+                }}
+            />
+          </View>
+          <View style={[styles.overlay, styles.overlay2, styles.overlay3]}>
+            {errorOccurred && ( // Afficher le texte d'erreur si une erreur est survenue
+              <Text style={styles.errorText}>Email ou Mot de passe incorrect</Text>
+            )}
           </View>
           <TouchableOpacity style={[styles.button, (isEmailValid && isPasswordValid) ? styles.buttonActive : null]} activeOpacity={0.7} onPress={() => login()} disabled={!isEmailValid || !isPasswordValid}>
               <Text style={[styles.buttonText, { fontWeight: 'bold' }]}>Se Connecter</Text>
@@ -142,6 +151,11 @@ const styles = StyleSheet.create({
   },
   overlay2: {
     top: '45%',
+    left: 0, // Center horizontally
+    right: 0, // Center horizontally
+  },
+  overlay3: {
+    top: '55%',
     left: 0, // Center horizontally
     right: 0, // Center horizontally
   },
@@ -212,4 +226,19 @@ const styles = StyleSheet.create({
   buttonActive: {
     backgroundColor: '#fff', // Change button color to white when both fields are valid
   },
+  errorInput: {
+    borderBottomColor: '#FF0000', // Changer la couleur du contour en rouge en cas d'erreur
+  },
+  errorText: {
+    color: '#FF0000',
+    fontSize: 16,
+    fontFamily: "roboto",
+    marginBottom: 10, // Espacement en bas pour éviter la superposition avec le bouton
+    top: '50%',
+    left: 0, // Center horizontally
+    right: 0, // Center horizontally
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
 });
